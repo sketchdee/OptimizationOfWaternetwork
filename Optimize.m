a34793efc672f43a090dccf30440f5a6d4c129cc
sketchdee <liujun18256329872@outlook.com>
2022-03-26 19:50:16 +0800

    problem fix

--- Optimize.m
+++ Optimize.m
@@ -1,11 +1,11 @@
-%处理约束
-
+%处理约束%
 m=12;
-n=15;
+n=12;
 
-FL=[53.3;2176;0;0;0;0;0;59;385;45;102;0];
+FL=[53.3;2176;26;0;0;0;0;59;385;45;102;2846.30];
+%简化模型
 
-%简化模型，不自身回用，两个末端单元，通过等式约束来控制。
+%不自身回用
 Aeq1=[];
 beq1=[];
 for i=1:1:m
@@ -15,7 +15,7 @@ for i=1:1:m
     beq1=[beq1;0];
 end
 
-%终端单元
+%2个终端单元
 for i=1:1:n
     M1=zeros(m,n);
     M1(10,i)=1;
@@ -39,141 +39,125 @@ for i=1:1:m
     end
     if(i==4)
         M1=zeros(m,n);
-        M1(4,5)=-1/0.432;
-        M1(1:9,4)=ones(9,1);
+        M1(4,5)=-1/0.431818;
+        M1(1:12,4)=ones(12,1);
         Aeq1=[Aeq1;Tool.M2V(M1)];
         beq1=[beq1;0];
     end
 end
-
 for i=1:1:m
-    if(i~=4)
+    if(i~=6)
         M1=zeros(m,n);
         M1(i,7)=1;
         Aeq1=[Aeq1;Tool.M2V(M1)];
         beq1=[beq1;0];
     end
-    if(i==4)
+    if(i==6)
         M1=zeros(m,n);
         M1(6,7)=-1/0.008;
-        M1(1:9,6)=ones(9,1);
+        M1(1:12,6)=ones(12,1);
         Aeq1=[Aeq1;Tool.M2V(M1)];
         beq1=[beq1;0];
     end
 end
 
-Cw=[18.9,0,133];
-%新鲜水负荷
-M1(12,13)=1;
-Aeq1=[Aeq1;Tool.M2V(M1)];
-beq1=[beq1;Cw(1)];
-M1(12,14)=1;
-Aeq1=[Aeq1;Tool.M2V(M1)];
-beq1=[beq1;Cw(2)];
-M1(12,15)=1;
-Aeq1=[Aeq1;Tool.M2V(M1)];
-beq1=[beq1;Cw(3)];
-
-
 %流量守恒约束
 Aeq2=[];
 beq2=[];
-for i=1:1:m
+for i=1:1:m-1
     M1=zeros(m,n);
-    M1(i,:)=ones(1,n).*(-1);
+    M1(i,:)=ones(1,n)*(-1);
     M1(:,i)=ones(m,1);
     M1(i,i)=0;
     Aeq2=[Aeq2;Tool.M2V(M1)];
-    beq2=[beq2;FL(i,1)]; 
+    beq2=[beq2;FL(i,1)];
 end
 
+%锅炉用水约束
+Q_boiler=110;
+for i=1:1:m
+    if(i~=6)
+        M1=zeros(m,n);
+        M1(i,8)=1;
+        Aeq2=[Aeq2;Tool.M2V(M1)];
+        beq2=[beq2;0];
+    end
+    if(i==6)
+        M1=zeros(m,n);
+        M1(i,8)=1;
+        Aeq2=[Aeq2;Tool.M2V(M1)];
+        beq2=[beq2;110];
+    end
+end
 
-%不等式约束
-CoutMax=[300	30	1000
-1600	100	28000
-1600	100	28000
-200	1000	0
-300	0	28000
-0.1	0	500
-1000	0	500
-10	0	2000
-20000	20000	0
-];
-ub=ones(m,n).*500;
-ub(1:9,13:15)=CoutMax;
-ub=Tool.M2V(ub);
-lb=Tool.M2V(zeros(m,n));
+%生活用水只使用新鲜水和除盐水
+for i=1:1:m
+    if(i==6)
+        continue;
+    end
+    if(i==12)
+        continue;
+    end
+    M1=zeros(m,n);
+    M1(i,1)=1;
+    Aeq2=[Aeq2;Tool.M2V(M1)];
+    beq2=[beq2;0];
+end
 
 Aeq=[Aeq1;Aeq2];
 beq=[beq1;beq2];
+ub_M=ones(m,n)*500;
+ub_M(:,2)=ones(m,1)*(2500);
+ub=Tool.M2V(ub_M)';
+lb=Tool.M2V(ones(m,n)*-1e-10)';
 
-x0=ones(1,m*n);
+Aineq=[];
+bineq=[];
+%最大入口流量约束
+%生活用水小于100
+Fmax=[100;3500; 300;300;300;1000;1000;1000;1000];
+for i=1:1:9
+    M1=zeros(m,n);
+    M1(i,:)=ones(n,1);
+    Aineq=[Aineq;Tool.M2V(M1)];
+    bineq=[bineq;Fmax(i,1)];
+end
+
+
+%去除多余的等式约束
+Aeq_beq=[Aeq,beq];
+if(rank(Aeq)~=rank(Aeq_beq))
+    disp('存在冲突的等式约束，无可行解，请检查等式约束！');
+end
+T= rref([Aeq_beq]);
+r=rank(T);
+Aeq=T(1:r,1:144);
+beq=T(1:r,145);
+
 options = optimoptions('fmincon');
-options = optimoptions(options,'Display', 'iter',...
-    'FunctionTolerance',1e-100,...
-    'MaxIterations', 100000,...
-    'Algorithm', 'active-set ',...
-    'FiniteDifferenceType', 'central',...
-    'StepTolerance',1e-1000);
-options.MaxFunctionEvaluations = 100000;
+options = optimoptions(options,...
+    'ConstraintTolerance',1e-10,...
+    'Display', 'iter',...
+    'FiniteDifferenceType','central',...
+    'FunctionTolerance',1e-10,...
+    'MaxFunctionEvaluations',2e5,...
+    'MaxIterations', 20000,...
+    'ObjectiveLimit',-1e100,...
+    'OptimalityTolerance',1e-10,...
+    'StepTolerance',1e-500,...
+    'UseParallel',false,...
+    'Algorithm', 'interior-point',...
+    'TolConSQP',1e-100);
 
-[x,fval,exitflag,output] = fmincon(@ObjectFunction,x0,[],[],Aeq,beq,lb,ub,@nonlcon);
-for i=1:1:100
-    x0=x;
-    [x,fval,exitflag,output] = fmincon(@ObjectFunction,x0,[],[],Aeq,beq,lb,ub,@nonlcon);
+x0=ones(1,144)*6.37;
+%[x,fval,exitflag,output] = fmincon(@ObjectFunction,x0,Aineq,bineq,Aeq,beq,lb,ub,[],options);
+%result=Tool.V2M(x,m,n);
 
+%%流量守恒测试函数
+function [Z]=ObjectFunction1(x)
+M= Tool.V2M(x,12,12);
+Z=0;
+for i=1:1:11
+    Z=Z+M(12,i);
 end
-
-%x = ga(@ObjectFunction,180,[],[],Aeq,beq,lb,ub,nonlcon)
-%result=reshape(x,[m,n])
-
-%杂质守恒约束
-%由于初始的出口浓度为未知的，所以只能通过联立方程组来求解
-%非线性等式约束
-function [c,ceq]=nonlcon(x)
-m=12;
-n=15;
-%M=ones(12,15);
-%M=Tool.V2M(x',m,n);
-M=reshape(x,[12,15]);
-MF=[
-314.28	237.6	0
-76750.32	20220	265165
-15445.5	0	89355
-22880	110000	-216480
-22880	-2850	383040
--33737.5	0	33000
-2149.9	0	264
-244	0	78336
-285263.3	65877	-283077
-];
-CinMax=[
-100	0	300
-300	10	2000
-300	10	2000
-200	500	2000
-200	300	0
-300	0	300
-0.1	0	300
-0.1	0	500
-1000	1000	2000
-];
-for i=1:1:9
-     ceq(i)=M(1:9,i)'*diag(ones(1,9)*(-1))*M(1:9,13) + M(i,1:12)*ones(12,1)*M(i,13)+M(12,i)*M(12,13)-MF(i,1);
-% ceq(1+3*(i-1))=M(1:9,i)'*diag(ones(1,9)*(-1))*M(1:9,13) + M(i,1:12)*ones(12,1)*M(i,13)+M(12,i)*M(12,13)-MF(i,1);
-% ceq(2+3*(i-1))=M(1:9,i)'*diag(ones(1,9)*(-1))*M(1:9,14) + M(i,1:12)*ones(12,1)*M(i,14)+M(12,i)*M(12,14)-MF(i,2);
-% ceq(3+3*(i-1))=M(1:9,i)'*diag(ones(1,9)*(-1))*M(1:9,15) + M(i,1:12)*ones(12,1)*M(i,15)+M(12,i)*M(12,15)-MF(i,3);
-end
-
-for i=1:1:9
-    c(i)= M(1:9,i)'*(M(1:9,13)-CinMax(1:9,1)) +M(12,i)*M(12,13);
-%c(1+3*(i-1))= M(1:9,i)'*(M(1:9,13)-CinMax(1:9,1)) +M(12,i)*M(12,13);
-%c(2+3*(i-1))= M(1:9,i)'*(M(1:9,13)-CinMax(1:9,2)) +M(12,i)*M(12,14);
-%c(3+3*(i-1))= M(1:9,i)'*(M(1:9,13)-CinMax(1:9,3)) +M(12,i)*M(12,15);
-end
-end
-
-function [Z]=ObjectFunction(x)
-    Z=x(166)+x(167)+x(168)+x(169)+x(170)+x(171)+x(172)+x(173)+x(174)+x(175)+x(176);
-
 end
--- Result_Check.m
+++ Result_Check.m
@@ -1,3 +1,6 @@
+clc;
+%保留一位小数
+ x=round(x,1);
 %%%%%%%%%优化结果检测%%%%%%%%%
 %----------------杂质负荷数据--------------------
 MF=[
@@ -42,7 +45,7 @@ CoutMax=[
 %---------------向量转矩阵形式------------------
 M= Tool.V2M(x,12,12);
 %最大允许误差，计算机浮点不存在完全的精确
-tol=1/1000;
+tol=1/10000000;
 %---------------------end--------------------------
 
 %-------------计算单元进出口浓度----------------
@@ -61,82 +64,108 @@ end
 b1=MF(:,1)+M(12,1:9)'*Cw(1);
 Ainv=inv(A);
 C1_out=Ainv*b1;
+%+----+----+----+----+
+%|unit| Cl | SS | SO4|
+%+----+----+----+----+
+%| 4  |  × |  √ |  √ |
+%+----+----+----+----+
+%| 6  |  √ |  × |  √ |
+%+----+----+----+----+
+%| 9  |  × |  × |  √ |
+%+----+----+----+----+
+%+-----------+-------------------+-----------+
+%|   Cl      |         SS        |    SO4    |
+%+-----------+-------------------+-----------+
+% 15085.4            -6600          -169840
+%杂质1
 %如果出现负浓度，则修正杂质负荷使得出口浓度为0
-A_sub=[];b_sub=[];
-if(C1_out(4,1)<0||C1_out(6,1)<0)
-    A_sub=[-Ainv(4,4),-Ainv(4,6);-Ainv(6,4),-Ainv(6,6)];
-    b_sub(1,1)=Ainv(4,[1,2,3,5,7,8,9])*b1([1,2,3,5,7,8,9],:);
-    b_sub(2,1)=Ainv(6,[1,2,3,5,7,8,9])*b1([1,2,3,5,7,8,9],:);   
-    C4=C1_out(4,1);
-    C6=C1_out(6,1);
-    if(C4<0)
-        C4=0;
-    end
-    if(C6<0)
-        C6=0; 
-    end
-    b_sub=b_sub-[C4;C6];
-    b1([4,6],1)=inv(A_sub)*b_sub;
-    C1_out=Ainv*b1;
+if(C1_out(6,1)<0)
+    C16min=0;
+    A_sub=A;
+    A_sub(6,:)=[];
+    A_sub(:,6)=[];
+    b_sub=b1([1:5,7:9],1)-C16min*A(6,6);
+    %计算其他浓度
+    C1_out_s=inv(A_sub)*b_sub;
+    C1_out(1:5,1)=C1_out_s(1:5,1);
+    C1_out(6,1)=C16min;
+    C1_out(7:9,1)=C1_out_s(6:8,1);
+    b1(6,1)=A(6,:)*C1_out;
 end
 C1sum=(C1_out'*M(1:9,1:9)+M(12,1:9)*Cw(1))';
 Qsum=(ones(1,12)*M(1:12,1:9))';
 C1_in=C1sum./Qsum;
 
+%杂质2
 b2=MF(:,2)+M(12,1:9)'*Cw(2);
 C2_out=inv(A)*b2;
 %如果出现负浓度，则修正杂质负荷使得出口浓度为0
-if(C2_out(4,1)<0||C2_out(6,1)<0)
-    A_sub=[-Ainv(4,4),-Ainv(4,6);-Ainv(6,4),-Ainv(6,6)];
-    b_sub(1,1)=Ainv(4,[1,2,3,5,7,8,9])*b2([1,2,3,5,7,8,9],:);
-    b_sub(2,1)=Ainv(6,[1,2,3,5,7,8,9])*b2([1,2,3,5,7,8,9],:);   
-    C4=C2_out(4,1);
-    C6=C2_out(6,1);
-    if(C4<0)
-        C4=0;
-    end
-    if(C6<0)
-        C6=0; 
-    end
-    b_sub=b_sub-[C4;C6];
-    b2([4,6],1)=inv(A_sub)*b_sub;
-    C2_out=Ainv*b2;
+if(C2_out(4,1)<0)
+    C24min=0;
+    A_sub=A;
+    A_sub(4,:)=[];
+    A_sub(:,4)=[];
+    b_sub=b2([1:3,5:9],1)-C24min*A(4,4);
+    %计算其他浓度
+    C2_out_s=inv(A_sub)*b_sub;
+    C2_out =[C2_out_s(1:3,1);C24min;C2_out_s(4:8,1);];
+    b2(4,1)=A(6,:)*C2_out;
 end
 C2sum=(C2_out'*M(1:9,1:9)+M(12,1:9)*Cw(2))';
 C2_in=C2sum./Qsum;
 
+%杂质3
 b3=MF(:,3)+M(12,1:9)'*Cw(3);
 C3_out=inv(A)*b3;
 %如果出现负浓度，则修正杂质负荷使得出口浓度为0
-%杂质3处理单元包括脱硫用水系统
 if(C3_out(4,1)<0||C3_out(6,1)<0||C3_out(9,1)<0)
-    A_sub=[-Ainv(4,4),-Ainv(4,6),-Ainv(4,9);
-        -Ainv(6,4),-Ainv(6,6),-Ainv(6,9);
-        -Ainv(9,4),-Ainv(9,6),-Ainv(9,9);];
-    b_sub(1,1)=Ainv(4,[1,2,3,5,7,8])*b3([1,2,3,5,7,8],:);
-    b_sub(2,1)=Ainv(6,[1,2,3,5,7,8])*b3([1,2,3,5,7,8],:);
-    b_sub(3,1)=Ainv(9,[1,2,3,5,7,8])*b3([1,2,3,5,7,8],:);
-    C4=C3_out(4,1);
-    C6=C3_out(6,1);
-    C9=C3_out(9,1);
-    if(C4<0)
-        C4=0;
+    C34min=0;
+    C36min=0;
+    C39min=0;
+    A_sub=A;
+    A_sub([4,6,9],:)=[];
+    A_sub(:,[4,6,9])=[];
+    b_sub=b3;
+    b_sub([4,6,9],:)=[];
+    rang=[1,2,3,5,7,8];
+    b_sub=b_sub-A(rang,4)*C34min-A(rang,6)*C36min-A(rang,9)*C39min;
+    C3_out_s=inv(A_sub)*b_sub;
+    C3_out(rang)=C3_out_s;
+    C3_out([4,6,9],:)=[0;0;0];
+    b3([4,6,9],:)=A([4,6,9],:)*C3_out;
+
+    %计算实际移除负荷
+    %确定最大移质位置
+    MF_3=b3-M(12,1:9)'*Cw(3);
+    rang_b=[];
+    if(MF_3(4,1)<MF(4,3))
+        rang_b=[rang_b,4];
     end
-    if(C6<0)
-        C6=0; 
+    if(MF_3(6,1)<MF(6,3))
+        rang_b=[rang_b,6];
     end
-    if(C9<0)
-        C9=0; 
+    if(MF_3(9,1)<MF(9,3))
+       rang_b=[rang_b,9];
     end
-    b_sub=b_sub-[C4;C6;C9];
-    b3([4,6,9],1)=inv(A_sub)*b_sub;
-    C3_out=Ainv*b3;
+    if(~isempty(rang_b))
+        rang_c=setdiff([4,6,9],rang_b);
+        A_sub=A;
+        A_sub(rang_c,:)=[];
+        A_sub(:,rang_c)=[];
+        b_sub=MF(:,3)+M(12,1:9)'*Cw(3);
+        b_sub(rang_c,:)=[];
+        C3_out_s=inv(A_sub)*b_sub;
+        rang_out_c=setdiff(1:9,rang_c);
+        C3_out(rang_out_c)=C3_out_s;
+        C3_out(rang_c,:)=ones(size(rang_c,2),1)*0;   %%TODO: the minimize c in each unit must be spetified.
+        b3=A*C3_out;
+    end
 end
 C3sum=(C3_out'*M(1:9,1:9)+M(12,1:9)*Cw(3))';
 C3_in=C3sum./Qsum;
 %---------------------end--------------------------
 
-%-----------进出口浓度超出限制加罚--------------
+%---------------进出口浓度超出限制加罚---------------
 %罚系数
 a=1000;
 Z=0;
@@ -222,6 +251,7 @@ C10_in3=(C3_out'*M(1:9,10)+M(12,10)*Cw(3))/F10sum;
 %———————end———————————
 
 %——————约束检查部分————————
+fprintf("报告生成日期：%s\n",datetime());
 %%% 计算结果汇总
 Cin=[C1_in,C2_in,C3_in];
 Cout=[C1_out,C2_out,C3_out];
@@ -230,23 +260,81 @@ C11_in=[C11_in1;C11_in2;C11_in3];
 
 fprintf('新鲜水用量：%s\n',num2str(roundn(M(12,:)*ones(12,1),-3)));
 fprintf('实际目标函数值：%s\n',num2str(roundn(Z,-3)));
+fprintf('总连接数；%d\n',sum(sum(roundn(M,-2)~=0)))
 
 %%%杂质负荷检查
 Fin=M'*ones(12,1);
 Fout=M*ones(12,1);
 MF_S=Cout.*Fout(1:9,1) - Cin.*Fin(1:9,1);
-disp("杂质负荷超限制值:")
+disp("杂质负荷超限制值:(实际 - 最大 < 0)")
+heade=[
+    '+-----------+-------------------+-----------+',
+    '|   Cl      |         SS        |    SO4    |',
+    '+-----------+-------------------+-----------+'
+];
+disp(heade)
 disp(num2str(roundn(MF_S - MF,-3)))
 
 %%% 极限进出口浓度检查
-disp('出口浓度超限制值：')
+disp('出口浓度超限制值：（实际 - 最大 < 0）')
+disp(heade)
 disp(num2str(roundn([C1_out-CoutMax(:,1),C2_out-CoutMax(:,2),C3_out-CoutMax(:,3)],-3)));
-disp('入口浓度超限制值：')
+disp('入口浓度超限制值：（实际 - 最大 < 0）')
+disp(heade)
 disp(num2str(roundn([C1_in-CinMax(:,1),C2_in-CinMax(:,2),C3_in-CinMax(:,3)],-3)));
 
 %%% 处理单元实际负荷，单元4，6，9
-disp("处理单元实际杂质负荷")
+disp("处理单元实际杂质负荷：（实用值）")
+disp(heade)
 disp(num2str(roundn(MF_S([4,6,9],:),-3)));
-disp("处理单元设计杂质负荷")
+
+disp("处理单元设计杂质负荷：（可用值）")
+disp(heade)
 disp(num2str(roundn(MF([4,6,9],:),-3)));
+
+%%生成报表
+id = [1:11]';
+C=[[C1_in;C10_in(1);C11_in(1)],...
+                  [C1_out;NaN;NaN],...
+                  [C2_in;C10_in(2);C11_in(2)],...
+                  [C2_out;NaN;NaN],...
+                  [C3_in;C10_in(3);C11_in(3)], ...
+                  [C3_out;NaN;NaN]];
+
+ReportTable_C=table(id,...
+    [C1_in;C10_in(1);C11_in(1)],...
+    [C1_out;NaN;NaN],...
+    [C2_in;C10_in(2);C11_in(2)],...
+    [C2_out;NaN;NaN],...
+    [C3_in;C10_in(3);C11_in(3)], ...
+    [C3_out;NaN;NaN], ...
+    'VariableNames',["ID","Cl_in","Cl_out","SS_in","SS_out","SO4_in","SO4_out"])
+
+ReportTable_C_rate=table(id,...
+    [C1_in;C10_in(1);C11_in(1)]./[CinMax(:,1);NaN;C11_inMax(1)],...
+    [C1_out;NaN;NaN]./[CoutMax(:,1);NaN;NaN],...
+    [C2_in;C10_in(2);C11_in(2)]./[CinMax(:,2);NaN;C11_inMax(2)],...
+    [C2_out;NaN;NaN]./[CoutMax(:,2);NaN;NaN],...
+    [C3_in;C10_in(3);C11_in(3)]./[CinMax(:,3);NaN;C11_inMax(3)], ...
+    [C3_out;NaN;NaN]./[CoutMax(:,3);NaN;NaN], ...
+    'VariableNames',["ID","Cl_in","Cl_out","SS_in","SS_out","SO4_in","SO4_out"])
+
+ReportTable_M=table([1:9]',...
+    MF_S(:,1),MF(:,1), ...
+    MF_S(:,2),MF(:,2), ...
+    MF_S(:,3),MF(:,3), ...
+    'VariableNames',["ID","Cl_m","Cl_M","SS_m","SS_M","SO4_m","SO4_M"])
+
+ReportTable_M_rate=table([1:9]',...
+    MF_S(:,1)./MF(:,1), ...
+    MF_S(:,2)./MF(:,2)+tol, ...
+    MF_S(:,3)./MF(:,3)+tol, ...
+    'VariableNames',["ID","Cl ","SS","SO4"])
+
+% clearvars -except x0 Aineq bineq Aeq beq lb ub options...
+%     ReportTable_M_rate ReportTable_M ReportTable_C_rate ReportTable_C...
+%     Cin Cout C10_in C11_in Fin Fout Z MF_S x
+
+
+
 
